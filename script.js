28da3e390695c704091afb37c7863467b72e06ad
Hewrxque <josehenriquevieiradacruz@gmail.com>
2022-12-25 16:48:55 -0300

    update style

--- script.js
+++ script.js
@@ -4,20 +4,26 @@ var nota3;
 var nota4;
 var resultado;
 
-function calcular(event){
+function calcular(event) {
     event.preventDefault();
-    nota1 = document.getElementById('nota1').value;
-    nota2 = document.getElementById('nota2').value;
-    nota3 = document.getElementById('nota3').value;
-    nota4 = document.getElementById('nota4').value;
     
+    nota1 = document.getElementById("nota1").value;
+    nota2 = document.getElementById("nota2").value;
+    nota3 = document.getElementById("nota3").value;
+    nota4 = document.getElementById("nota4").value;
+
     media = (nota1 + nota2 + nota3 + nota4) / 4;
-    resultado = document.getElementById('resultado');
-    if(media < 7){
-        resultado.innerHTML = '<br/> Seu resultado foi ' + media.toFixed(2) + '<br/> Você está reprovado';
-    } else if(media >=7){
-        resultado.innerHTML = '<br/> Seu resultado foi ' + media.toFixed(2) + '<br/> Você está aprovado, Parabéns';
-           
+    resultado = document.getElementById("resultado");
+    if (media < 7) {
+        resultado.innerHTML =
+            "<br/> Seu resultado foi " +
+            media.toFixed(2) +
+            "<br/> Você está reprovado";
+    } else if (media >= 7) {
+        resultado.innerHTML =
+            "<br/> Seu resultado foi " +
+            media.toFixed(2) +
+            "<br/> Você está aprovado, Parabéns";
     }
-    document.getElementById('media').value = '';
+    document.getElementById("media").value = "";
 }
