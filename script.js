dd97dfd2eed6593e352a5a12c9d814a7aa2cb5c1
Hewrxque <josehenriquevieiradacruz@gmail.com>
2022-12-26 18:53:13 -0300

    ok

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ var resultado;
 function calcular(event) {
     event.preventDefault();
     
-    nota1 = document.getElementById("nota1").value;
-    nota2 = document.getElementById("nota2").value;
-    nota3 = document.getElementById("nota3").value;
-    nota4 = document.getElementById("nota4").value;
+    nota1 = document.getElementById('nota1').value;
+    nota2 = document.getElementById('nota2').value;
+    nota3 = document.getElementById('nota3').value;
+    nota4 = document.getElementById('nota4').value;
 
     media = (nota1 + nota2 + nota3 + nota4) / 4;
-    resultado = document.getElementById("resultado");
+    resultado = document.getElementById('resultado');
     if (media < 7) {
         resultado.innerHTML =
             "<br/> Seu resultado foi " +
@@ -25,5 +25,4 @@ function calcular(event) {
             media.toFixed(2) +
             "<br/> Você está aprovado, Parabéns";
     }
-    document.getElementById("media").value = "";
 }
